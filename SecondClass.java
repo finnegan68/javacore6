c421b000d133ed25e455e03ce1cff8e91357993c
finnegan68 <jj221210@gmail.com>
2017-01-24 20:49:49 +0200

    homework #1

---
+++ SecondClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Илья on 24.01.2017.
+ */
+public class SecondClass {
+
+    public static void main(String[] args) {
+        System.out.println("My first programs on Java...");
+    }
+}
