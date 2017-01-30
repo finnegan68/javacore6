9ef22e941f87cd4e952aab2430c29d5bda3c1e79
finnegan68 <jj221210@gmail.com>
2017-01-30 23:47:06 +0200

    homework #2

---
+++ module2/Task2.java
@@ -0,0 +1,24 @@
+package module2;
+
+/**
+ * Created by Илья on 30.01.2017.
+ */
+public class Task2 {
+    public static void main(String[] args) {
+
+        System.out.println("********TEST1********");
+        withdraws(100,10);
+        System.out.println("********TEST2********");
+        withdraws(100,99);
+    }
+    public static void withdraws(double balance, double withdrawal){
+        double commision = withdrawal * 0.05;
+        double newBalance = balance - withdrawal - commision;
+        if (newBalance < 0){
+            System.out.println("NO");
+        }
+        else{
+            System.out.println("OK " + commision + " " + newBalance);
+        }
+    }
+}
