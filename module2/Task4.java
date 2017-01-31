8b850b25b516f41fbeeb548890681b335a6dc0bd
finnegan68 <jj221210@gmail.com>
2017-01-31 13:13:43 +0200

    homework #2

---
+++ module2/Task4.java
@@ -0,0 +1,20 @@
+package module2;
+
+/**
+ * Created by Илья on 31.01.2017.
+ */
+public class Task4 {
+    public static void main(String[] args) {
+
+        int[] balances = {1200, 250, 2000, 500, 3200};
+        String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+
+        for (int i = 0; i < balances.length; i++) {
+            fund(ownerNames[i], 490, balances[i]);
+        }
+    }
+    public static void fund(String ownerName, double withdrawal, double balance){
+        double newBalance = withdrawal + balance;
+        System.out.println(ownerName + " " + newBalance);
+    }
+}
