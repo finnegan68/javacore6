4dececa6d4a1bc5276294c0d07129e7ac0781746
finnegan68 <jj221210@gmail.com>
2017-01-31 13:06:28 +0200

    homework #2

---
+++ module2/Task3.java
@@ -0,0 +1,28 @@
+package module2;
+
+/**
+ * Created by Илья on 31.01.2017.
+ */
+public class Task3 {
+    public static void main(String[] args) {
+
+        int[] balances = {1200, 250, 2000, 500, 3200};
+        String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+
+        for (int i = 0; i < balances.length; i++) {
+            withdraw(ownerNames[i], 490, balances[i]);
+        }
+    }
+    public static void withdraw(String ownerName, int withdrawal, int balance){
+        double commision = withdrawal * 0.05;
+        double newBalance = balance - withdrawal - commision;
+        if (newBalance < 0){
+            System.out.println(ownerName + " NO");
+        }
+        else{
+            System.out.println(ownerName + " " + withdrawal + " " + newBalance);
+        }
+
+    }
+}
+
