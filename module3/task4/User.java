571e4a4f980e214df52d299ca032a9e5ceafb65f
finnegan68 <jj221210@gmail.com>
2017-02-10 14:40:04 +0200

    homework#3 task4

---
+++ module3/task4/User.java
@@ -0,0 +1,84 @@
+package module3.task4;
+
+
+public class User {
+    private String name;
+    private int balance;
+    private int monthsOfEmployment;
+    private String companyName;
+    private int salary;
+    private String currency;
+
+    public User(String name, int balance, int monthsOfEmployment, String companyName, int salary, String currency) {
+        this.name = name;
+        this.balance = balance;
+        this.monthsOfEmployment = monthsOfEmployment;
+        this.companyName = companyName;
+        this.salary = salary;
+        this.currency = currency;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public int getMonthsOfEmployment() {
+        return monthsOfEmployment;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public void setMonthsOfEmployment(int monthsOfEmployment) {
+        this.monthsOfEmployment = monthsOfEmployment;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+    public void paySalary(){
+        balance += salary;
+    }
+    public void withdraw(int summ){
+        if (summ < 1000){
+            balance -= summ * 1.05;
+        }else {
+            balance -= summ * 1.1;
+        }
+    }
+    public int companyNameLenght(){
+        return companyName.length();
+    }
+    public void  monthIncreaser(int addMonth){
+        monthsOfEmployment += addMonth;
+    }
+}
