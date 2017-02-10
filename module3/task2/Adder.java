ba6fcbf10344e600f1105481817c7a0f9e7ca63e
finnegan68 <jj221210@gmail.com>
2017-02-10 11:21:44 +0200

    homework#3 task2

---
+++ module3/task2/Adder.java
@@ -0,0 +1,13 @@
+package module3.task2;
+
+
+public class Adder extends Arithmetic {
+
+    public boolean check(Integer a, Integer b){
+        if (a > b){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
---
+++ module3/task2/Arithmetic.java
@@ -0,0 +1,10 @@
+package module3.task2;
+
+
+public class Arithmetic {
+
+    public int add(Integer a, Integer b){
+        Integer sum = a + b;
+        return sum;
+    }
+}
