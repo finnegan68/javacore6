c1248d0dec1817e411c1f63b175870ebb5033e8f
finnegan68 <jj221210@gmail.com>
2017-03-11 11:05:34 +0200

    homework#4 in new branch

---
+++ module5/Main.java
@@ -0,0 +1,8 @@
+package module5;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Controller myController = new Controller();
+    }
+}
---
+++ module5/TripAdvisorAPI.java
@@ -0,0 +1,26 @@
+package module5;
+
+
+import java.util.Arrays;
+
+public class TripAdvisorAPI implements API {
+
+    Room[] rooms = new Room[5];
+
+    @Override
+    public Room[] getRooms() {
+        return new Room[0];
+    }
+
+    @Override
+    public Room[] findRooms(int price, int persons, String city, String hotel) {
+        Room[] answ = new Room[0];
+        for (Room r : rooms){
+            if(r.getPrice() == price && r.getPersons() == persons && r.getCityName() == city && r.getHotelName() == hotel){
+                answ = Arrays.copyOf(answ, answ.length + 1);
+                answ[answ.length - 1] = r;
+            }
+        }
+        return answ;
+    }
+}
